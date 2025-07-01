86c4ccc6abef81f1108773038942894d92661024
Kenechukwu Nwafor <kenechukwunwafor@Kenechukwus-MacBook-Pro.local>
2025-07-01 20:08:23 +0100

    Added the server connections, mongoURI connections

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Notes API');
+})
+
+const PORT = process.env.PORT || 3500;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
