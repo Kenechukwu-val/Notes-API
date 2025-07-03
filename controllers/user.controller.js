22061d95feac02a94c4825b54cd76c8b67dd5d60
Kenechukwu Nwafor <kenechukwunwafor@Kenechukwus-MacBook-Pro.local>
2025-07-03 23:30:10 +0100

    Created a jwt token

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,11 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+// Function to generate JWT token
+const generateToken = (user) => {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return token;
+}
 
 exports.register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -12,7 +19,7 @@ exports.register = async (req, res) => {
         const user = await User.create({ name, email, password });
         res.status(201).json({
             message: 'User registered successfully',
-            user
+            User: generateToken(user)
         })
     }
     catch (err) {
@@ -27,6 +34,10 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
     const { email, password } = req.body;
 
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required' });
+    }
+
     try {
         //Checks if the user exists
         const user = await User.findOne({ email });
@@ -38,7 +49,7 @@ exports.login = async (req, res) => {
 
         res.status(200).json({
             message: 'Login successful',
-            user
+            user: generateToken(user)
         });
     }
     catch(err) {
